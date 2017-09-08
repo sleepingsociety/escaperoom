13910a1a401babac3f4b79675fc8375afdc8f50a
Wörk-PC\Lukas <lukasauriquio@gmail.com>
2017-09-08 14:19:59 +0200

    Created first Version of the Gamescreen. For now it can load an image.

--- src/Start.java
+++ src/Start.java
@@ -2,7 +2,15 @@
  * Created by david on 08.09.17.
  */
 
+import ch.tbz.game.CustomPanel;
 import javafx.application.Application;
+import javafx.embed.swing.JFXPanel;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Start extends Application {

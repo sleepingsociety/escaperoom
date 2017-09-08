e989a08036bc5c0caaceed21fe97fb31ff7b8de9
David Kalchofner <david@macbook-pro-DK.tbz.local>
2017-09-08 13:12:46 +0200

    added start.java

---
+++ src/Start.java
@@ -0,0 +1,18 @@
+/**
+ * Created by david on 08.09.17.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Start extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
